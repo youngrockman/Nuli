1e7b3707b7ab25678fe110311ffbb1e7389bf758
Your Name <you@example.com>
2024-12-04 11:54:31 +0300

    first commit

---
+++ Nuli/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите числа через пробел:");
+        string input = Console.ReadLine();
+
+        // Преобразуем строку в массив чисел
+        string[] parts = input.Split(' ');
+        int[] nums = new int[parts.Length];
+        for (int i = 0; i < parts.Length; i++)
+        {
+            nums[i] = int.Parse(parts[i]);
+        }
+
+        MoveZeroes(nums);
+
+        Console.WriteLine("Результат:");
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.Write(nums[i] + " ");
+        }
+    }
+
+    static void MoveZeroes(int[] nums)
+    {
+        int index = 0;
+
+        // Перемещаем ненулевые элементы
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] != 0)
+            {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        // Заполняем оставшиеся позиции нулями
+        for (int i = index; i < nums.Length; i++)
+        {
+            nums[i] = 0;
+        }
+    }
+}
